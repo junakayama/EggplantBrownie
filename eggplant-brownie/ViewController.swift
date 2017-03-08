8483714c6f3b85a2f768dd5ad5d6b128535a80b6
Júlia Nakayama Silveira <junakayama97@gmail.com>
2017-03-07 23:51:33 -0300

    first commit

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -9,17 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!
+    
+    @IBOutlet var happinessField: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func add() {
+        let name = nameField.text
+        let happines = happinessField.text
+        print("eaten \(name) with happiness \(happines)!")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
